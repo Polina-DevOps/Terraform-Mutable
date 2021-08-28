8a039e0a38408bb8ad512a28c83f3df246cc05c7
Nageswar Polina <polinadevops2021@gmail.com>
2021-08-28 23:27:04 +0800

    vpc create v1.1.6.7

--- vpc/env/dev.tfvars
+++ vpc/env/dev.tfvars
@@ -1,6 +1,7 @@
 VPC_PRIVATEIP_CIDR        = "10.0.0.0/16"
 PRIVATE_SUBNET_CIDR       = ["10.0.0.0/17", "10.0.128.0/17"]
+VPC_SUBNET_AZ             = ["us-east-1a", "us-east-1b"]
 VPC_PUBLICIP_CIDR         = "10.100.0.0/22"
 PUBLIC_SUBNET_CIDR        = ["10.100.0.0/23", "10.100.2.0/23"]
 ENV                       = "dev"
-VPC_SUBNET_AZ             = ["us-east-1a", "us-east-1b"]
+
--- vpc/subnet.tf
+++ vpc/subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "private" {
-  count                 = length(var.VPC_PRIVATEIP_CIDR)
+  count                 = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id                = aws_vpc.main.id
   cidr_block            = element(var.PRIVATE_SUBNET_CIDR,count.index )
 
@@ -9,7 +9,7 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_subnet" "public" {
-  count                 = length(var.VPC_PUBLICIP_CIDR)
+  count                 = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id                = aws_vpc.main.id
   cidr_block            = element(var.PUBLIC_SUBNET_CIDR,count.index )
 
