8664614702a65fa3ddb3a6308f27156af7c33173
Nageswar Polina <polinadevops2021@gmail.com>
2021-08-29 19:04:49 +0800

    vpc create v1.1.7.1

---
+++ Jenkinsfile
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'ACTION', choices: ['dev-apply', 'prod-apply'], description: 'Choose Environment')
+    }
+    stages {
+        stage('VPC Apply') {
+            steps {
+                sh '''
+          cd vpc
+          make ${ACTION}
+        '''
+            }
+        }
+        stage('DB Apply') {
+            steps {
+                sh '''
+          cd databases
+          make ${ACTION}
+        '''
+            }
+        }
+    }
+}
--- vpc/subnet.tf
+++ vpc/subnet.tf
@@ -20,12 +20,14 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_route_table_association" "public_rt" {
+  depends_on            = [aws_nat_gateway.natgw]
   count                 = length(var.PUBLIC_SUBNET_CIDR)
   subnet_id             = element(aws_route_table.public.*.id,count.index )
   route_table_id        = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private_rt" {
+  depends_on            = [aws_nat_gateway.natgw]
   count                 = length(var.PRIVATE_SUBNET_CIDR)
   subnet_id             = element(aws_route_table.private.*.id,count.index )
   route_table_id        = aws_route_table.private.id
