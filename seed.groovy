2b66d238b0cfc6d8f95faedb759c64f962fd0e1f
Nageswar Polina <polinadevops2021@gmail.com>
2021-08-29 20:20:46 +0800

    vpc create v1.1.7.4

---
+++ seed.groovy
@@ -0,0 +1,30 @@
+folder('CI-VPC') {
+    displayName('CI-VPC')
+    description('CI-VPC')
+}
+
+    pipelineJob("CI-VPC/${j}") {
+        configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/Polina-DevOps/Terraform-Mutable.git'+j)
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+}
